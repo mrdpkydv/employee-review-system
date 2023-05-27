28501a66d6522beb005f4a79537ab08a6852460d
mrdpkydv <132705827+mrdpkydv@users.noreply.github.com>
2023-05-28 02:10:07 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+
+mongoose.connect('mongodb+srv://mrdpkydv:Deepak%401314@cluster0.wvxaikj.mongodb.net/employee?retryWrites=true&w=majority');
+// mongoose.connect('mongodb://localhost/employee-review-system');
+
+const db = mongoose.connection;
+
+//if error then pritnt message
+db.on('error', console.error.bind(console, 'error in connectin DB'));
+
+// server is up then run a message 
+db.once('open', () => {
+    console.log('Succesfully !! Connected to the DataBase');
+})
+module.export = db;
